3af2151f606df398162a6afa05c2345840f5ad60
Max <prizrakx@yandex.ru>
2020-06-24 17:39:39 +0300

    Created an application framework

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// Parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index page
+app.get("/", (req, res) => {
+  res.send("Welcome to Stolen Bike Cases application.");
+});
+
+// Set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
